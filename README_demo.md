71a85748adf85a056ad5be89b3e9ee4eeef2b716
josemelendez77 <141665270+josemelendez77@users.noreply.github.com>
2023-11-07 02:02:05 -0700

    final commit

---
+++ README_demo.md
@@ -0,0 +1,35 @@
+# Readme-generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+This project generates a readme using node.js. It generates it by asking the user a couple questions.  
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+
+## Installation
+ 
+
+## Usage
+Fork the repo, run 'npm init', run 'npm install inquirer'
+
+## Contributing
+
+
+## Tests
+no tests
+
+## Questions
+Contact email: melendezjose585@gmail.com
+
+GitHub: [josemelendez77](https://github.com/josemelendez77)
+
+
+
+## License
+This project is licensed under the terms of the MIT license.
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,59 @@
+const badge = require('./licenseBadge');
+
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+${badge[data.license]}
+
+## Description
+${data.description}  
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+
+## Installation
+${data.installation} 
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Contact email: ${data.email}\n
+GitHub: [${data.github}](https://github.com/${data.github})\n
+${data.addlquestions}
+
+## License
+This project is licensed under the terms of the ${data.license} license.
+`;
+}
+
+module.exports = generateMarkdown;
+
+/* README Format from Good-README-Guide
+Title
+License badge
+  https://shields.io/category/license
+  https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+Description
+Table of contents (Links)
+Sections:
+  Installation
+  Usage
+  Contributing
+  Tests
+  Questions
+    Email
+    GitHub Profile link
+  License
+*/
